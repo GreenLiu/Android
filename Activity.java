f66ab81d39c0afe72b1b8d6789a54073721340de
GreenLiu <greenliu@msn.com>
2017-09-15 19:41:29 +0800

    Update Activity.java

--- Activity.java
+++ Activity.java
@@ -1,10 +1,32 @@
 public class Activity extends ContextThemeWrapper implements LayoutInflater.Factory2, Window.Callback, KeyEvent.Callback, 
-      							     OnCreateContextMenuListener, ComponentCallbacks2 
+      							                             OnCreateContextMenuListener, ComponentCallbacks2 
 {
-	
+    private Application mApplication;
+    /*package*/ Intent mIntent;
+    Activity mParent;
+
+    /*package*/ boolean mResumed;
+    private boolean mStopped;
+    boolean mFinished;
+    boolean mStartedActivity;
+    private boolean mDestroyed;
+
+    private SearchManager mSearchManager;
+    private MenuInflater mMenuInflater;
+
+    private Window mWindow;
+    private WindowManager mWindowManager;
+
+    /*package*/ View mDecor = null;
+    
+    private Thread mUiThread;
+    final Handler mHandler = new Handler();
+    
+    protected void onCreate(Bundle savedInstanceState) 
+    {
 	
 
-
+    }
 
 
 
