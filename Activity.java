c04a635b131e7a2cc025ca2b3b710302b0743c76
GreenLiu <greenliu@msn.com>
2017-09-15 18:53:53 +0800

    Update Activity.java

--- Activity.java
+++ Activity.java
@@ -1,5 +1,5 @@
 public class Activity extends ContextThemeWrapper implements LayoutInflater.Factory2, Window.Callback, KeyEvent.Callback, 
-        													 OnCreateContextMenuListener, ComponentCallbacks2 
+      							    						 OnCreateContextMenuListener, ComponentCallbacks2 
 {
 	
 	
