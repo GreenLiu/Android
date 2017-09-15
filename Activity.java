23bb3707e924fc708517568ba1a7ed11dd49c9e1
GreenLiu <greenliu@msn.com>
2017-09-15 18:52:41 +0800

    Add files via upload

---
+++ Activity.java
@@ -0,0 +1,12 @@
+public class Activity extends ContextThemeWrapper implements LayoutInflater.Factory2, Window.Callback, KeyEvent.Callback, 
+        													 OnCreateContextMenuListener, ComponentCallbacks2 
+{
+	
+	
+
+
+
+
+
+
+}
